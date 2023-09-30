ed092904a2d6077e3327eb5eaba7987f6f444c83
jennifertramsu <jennifer.tramsu@gmail.com>
2023-09-30 10:38:45 -0400

    adding a4:

---
+++ A4/scripts/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+path=../data
+file=$path/nyc_311_limit
+
+# Extracting relevant columns
+csvtool col 2-4,6,9,20,26 $file.csv > $path/${file}_filtered.csv
+
+# Creating csv with headers
+cat $path/columns.txt > $path/${file}_filtered_columns.csv
+cat $path/${file}_filtered.csv >> $path/${file}_filtered_columns.csv
+
+# Filtering for 2020
+csvgrep -c 1 -r "[01].\/[01]./2020" $path/${file}_filtered_columns.csv > $path/${file}_2020.csv
